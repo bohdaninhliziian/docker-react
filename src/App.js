27ee049c958c1baa3bf651be25e791e33c8500c4
bohdaninhliziian <bohdan.inhliziian@gmail.com>
2022-10-07 11:58:19 +0200

    expose push feature

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          i was changed on feature branch
         </a>
       </header>
     </div>
